6cde708d8d6c06a97de3db06a1b5f80ca9c7035d
farit <faritteranacosta@gmail.com>
2025-03-21 23:10:00 -0500

    Refactor card shuffling logic and enhance card rendering in JavaScript

--- js/index.js
+++ js/index.js
@@ -1,25 +1,32 @@
 const cards = ["baggio", "beckham", "casillas", "cr7", "Cruyff", "eto", "messi", "neymar", "ronaldinho", "sergio_ramos"]
 
-console.log(cards.sort(function(){return 0.5 - Math.random()}));
+function desordenar(){
+    for (let i = cards.length -1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i+1));
+        let k = cards[i];
+        cards[i] = cards[j];
+        cards[j] = k;
+      }
 
-for (let i = 0; i < cards.length; i++) {
-    const card = document.createRange().createContextualFragment(/*html*/`
-        <div class="container"> <div class="card"><img src="resorces/${cards[i]}.webp" alt="img-${cards[i]}"></div></div>
-    `);
-
-    const section = document.querySelector("section");
-    section.append(card);
+    return cards;
 }
 
-console.log(cards.sort(function(){return 0.5 - Math.random()}));
+console.log(desordenar());
 
+var count = 0;
 for (let i = 0; i < cards.length; i++) {
     const card = document.createRange().createContextualFragment(/*html*/`
         <div class="container"> <div class="card"><img src="resorces/${cards[i]}.webp" alt="img-${cards[i]}"></div></div>
     `);
-
     const section = document.querySelector("section");
     section.append(card);
+
+    if (i == cards.length-1 && count < 1) {
+        i = -1;
+        console.log(i);
+        count++;
+        console.log(desordenar());  
+    }
 }
 
 
