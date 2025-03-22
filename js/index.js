2ec55d930b588df1aa7c7dcde64e9c20ef787580
farit <faritteranacosta@gmail.com>
2025-03-22 13:28:42 -0500

    Remove unused image resources and update HTML structure for card layout

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const cards = ["baggio", "beckham", "casillas", "cr7", "Cruyff", "eto", "messi", "neymar", "ronaldinho", "sergio_ramos"]
+const desorden = desordenar().slice();
+console.log(cards);
 
 function desordenar(){
     for (let i = cards.length -1; i > 0; i--) {
@@ -11,23 +13,29 @@ function desordenar(){
     return cards;
 }
 
-console.log(desordenar());
-
-var count = 0;
-for (let i = 0; i < cards.length; i++) {
+for (let i = 0; i < cards.length*2; i++) {
     const card = document.createRange().createContextualFragment(/*html*/`
-        <div class="container"> <div class="card"><img src="resorces/${cards[i]}.webp" alt="img-${cards[i]}"></div></div>
+        <div class="container" onclick = "voltear(img${i+1})"> <div class="card"><img id = "img${i+1}" src="resources/carta.webp" alt="${i+1}"></div></div>
     `);
     const section = document.querySelector("section");
     section.append(card);
-
-    if (i == cards.length-1 && count < 1) {
-        i = -1;
-        console.log(i);
-        count++;
-        console.log(desordenar());  
-    }
 }
 
+var bandera = 1 ;
 
-
+function voltear(card){
+    if (card.alt-1 >= cards.length) {
+        if(bandera){
+            console.log(desordenar());
+            bandera = 0;
+        }
+        
+            console.log(card);
+            card.src = "resources/"+cards[card.alt-11]+".webp";
+            console.log(card.id)
+    }else{
+        console.log(card);
+        card.src = "resources/"+desorden[card.alt-1]+".webp";
+        console.log(card.id)
+    }    
+}
